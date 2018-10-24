6f5fc9f50142b02846363393966372a22375b9ec
Yifei Feng <yifeif@google.com>
2018-10-24 11:23:29 -0700

    Make sure coder ops can always be accessed.   - do not use contrib version of load_op_library, else contrib version of   coder_ops always gets loaded first.   - wrap module with try statement to make sure they are accessible even   if contrib version gets loaded first.

--- tensorflow_compression/python/ops/coder_ops.py
+++ tensorflow_compression/python/ops/coder_ops.py
@@ -24,10 +24,20 @@ from tensorflow.python.framework import load_library
 from tensorflow.python.platform import resource_loader
 
 
+# Use framework version of load_op_library instead of contrib version,
+# otherwise if tensorflow contains coder_ops, it will always get loaded
+# before tensorflow_compression version can be loaded.
 coder_ops = load_library.load_op_library(
     resource_loader.get_path_to_datafile("_coder_ops.so"))
 
+# When tf.contrib gets loaded before tensorflow_compression, the coder_ops
+# module returned by load_op_library will contain no ops because they have
+# already be registered earlier. In this case, use tf.contrib version instead.
+try:
+  pmf_to_quantized_cdf = coder_ops.pmf_to_quantized_cdf
+except AttributeError:
+  from tensorflow.contrib.coder.python.ops import coder_ops
+  pmf_to_quantized_cdf = coder_ops.pmf_to_quantized_cdf
 
-pmf_to_quantized_cdf = coder_ops.pmf_to_quantized_cdf
 range_decode = coder_ops.range_decode
 range_encode = coder_ops.range_encode
